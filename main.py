a207e189e0c2ee49cef363eb12b0a56680203a8d
FARHAN MAHAMUD <s331381228@ocdsb>
2018-03-28 10:06:47 -0400

    alsdfhsdljfh

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # Assignment 1
 # ICS3U
-# <your name>
+# <Farhan Mahamud>
 # March 28, 2018
 
 ###### uncomment this when you are ready to work on it
-#def CtoF ():
-#
+def CtoF (C):
+    """ C = Celcius, this function is used to transform degrees celcius into degrees ferenheit"""
+    F= (1.8)*C+32
+    return F
+
 
 ###### uncomment this when you are ready to work on it
 #def FtoC ():
 #
 
-temperature = int(input('Enter your temperature in Celsius: '))
-print(temperature)
+#temperature = int(input('Enter your temperature in Celsius: '))
+#print(temperature)
